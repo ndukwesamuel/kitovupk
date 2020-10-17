cecf469b2ec236034984a50510a5dfc5f4bb8a02
ndukwesamuel23 <ndukwesamuel23@gmail.com>
2020-10-17 12:32:51 +0100

    this is the firts commit

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+
+
+	context = {
+		
+	}
+	return render(request, 'index.html', context)
+
+
+
+def nav(request):
+
+
+	context = {
+		
+	}
+	return render(request, 'nav.html', context)
+
