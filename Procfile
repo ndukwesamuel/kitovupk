067d29d71f7dee2685d1bc965605bc871cbf5642
ndukwesamuel23 <ndukwesamuel23@gmail.com>
2020-10-17 13:05:15 +0100

    this is the second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kitovu.wsgi  --log-file -
--- kitovu/settings.py
+++ kitovu/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'kq8arp*5^8pf^v5z$&p!ef6jnvy!u3jmvgnl4)&bs-ow1f(=9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kitovu.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
